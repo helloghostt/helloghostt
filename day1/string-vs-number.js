675d783b3c55a95e847d81bee97b0cfe5b0e3281
Anastacia(박초롱) <155081933+helloghostt@users.noreply.github.com>
2024-06-27 16:19:00 +0900

    Delete day1 directory

--- day1/string-vs-number.js
+++
@@ -1,9 +0,0 @@
-// let a = 5;
-// let b = "6";
-// console.log(a + b);
-// let a: number = 5;
-// let b: string = "6";
-// const result: string = a.toString() + b;
-var a = 5;
-var b = 6;
-console.log(a + b);
